2abe62d4e5c877c6e23663bd4e996be6cc776e21
TAKAHASHI Shuuji <shuuji3@gmail.com>
2022-01-01 18:46:28 +0900

    Write run.bash to reproduce the experiment

---
+++ ca-bundle/run.bash
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# Get curl's script to convert Mozilla's ca-bundle to the pem format.
+wget https://raw.githubusercontent.com/curl/curl/master/lib/mk-ca-bundle.pl
+
+# Download and create a CA bundle.
+perl mk-ca-bundle.pl
